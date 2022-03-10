ac7bd1b8b0ac6e3a9675ebe5d57ea5d556717ce6
rodsan05 <rodsan05@ucm.es>
2022-03-10 20:14:35 +0100

    limpia de archivos innecesarios

--- TPV2/TPV2/src/game/DeAcceleration.cpp
+++
@@ -1,8 +0,0 @@
-#include "DeAcceleration.h"
-
-#include "../sdlutils/SDLUtils.h"
-#include "Container.h"
-void DeAcceleration::update(Container* o)
-{
-	o->getVel().set(o->getVel()*deaccelValue_);
-}
